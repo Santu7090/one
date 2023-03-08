f92b105121ea84608e60bb3731f75f7b7678bb1b
Santu7090 <119152955+Santu7090@users.noreply.github.com>
2023-03-08 14:45:12 +0530

    Python Calculator

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+num1 = int(input("Enter the first number : "))
+operator = input("Enter the operator(+,-,*,/,%) : ")
+num2 = int(input("Enter the second number : "))
+
+if operator == "+" :
+    print(num1+num2)
+
+elif operator == "-" :
+    print(num1-num2)
+
+elif operator == "*" :
+    print(num1*num2)
+
+elif operator == "/" :
+    print(num1/num2)
+
+elif operator == "%" :
+    print(num1%num2)
+
+else:
+    print("Invalid operator")
